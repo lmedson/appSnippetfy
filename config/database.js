bf70114430e257c43625752a572cb01a3c289375
lmedson <medson-mbp@hotmail.com>
2018-05-31 23:46:48 -0300

    Add views, auth and orm

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username:'root',
+  password:'senha123',
+  database: 'mysql',
+  host: '127.0.0.1',
+  dialect:'mysql',
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const routes = require('./app/routes/routes')
+
+const app = express();
+
+nunjucks.configure(path.resolve('app','views'),{
+  autoescape: true,
+  express: app
+});
+
+app.use(express.static(path.resolve('app','public')));
+
+app.set('view engine','njk'); // dessa forma não é necessário informar a extensão do arquivo no res.send
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+app.listen(3000);
