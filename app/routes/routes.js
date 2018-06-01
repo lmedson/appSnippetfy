2f2c175cded3187caeedf6bc7692179d830e1730
lmedson <medson-mbp@hotmail.com>
2018-06-01 20:49:10 -0300

    Add auth middleware

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const routes = express.Router();
 
-const authController = require('../controllers/authController.js'); 	  	
+const authMiddleware = require('../middlewares/auth');
+
+const authController = require('../controllers/authController'); 	  	
+const dashboardController = require('../controllers/dashboardController');
 
 routes.use((req,res,next ) => {
 	//locals allows access to views variables
@@ -9,10 +13,16 @@ routes.use((req,res,next ) => {
 	res.locals.flashError = req.flash('error');
 	next();
 });
+
 routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
+routes.get('/signout', authController.signout);
+
 
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);	
+
+routes.use('/app/', authMiddleware); //setting the auth middleware for all routes with prefix '/app'
+routes.get('/app/dashboard', dashboardController.index);
 
 module.exports =  routes;
