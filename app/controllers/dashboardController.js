6e06b79224d7d3ca9f8c2c609221cf9193ff0452
lmedson <medson-mbp@hotmail.com>
2018-06-10 21:57:08 -0300

    Insert categories and snippets

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,5 +1,17 @@
+const { Category, Snippet } = require('../models');
+
 module.exports = {
-	index(req,res) {
-		return res.render('dashboard/index');
-	},
+	async index(req,res,next) {
+    try {
+      const categories = await Category.findAll({
+        include: [Snippet], //if category is related to snippet, call all snippets by category
+        where: {
+          UserId: req.session.user.id,
+      },
+    });
+      return res.render('dashboard/index', { categories });
+    } catch(err) {
+      return next(err);
+    }
+  },
 };
